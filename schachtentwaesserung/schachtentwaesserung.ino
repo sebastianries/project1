07dd5a6c9b905150f0061452c82c59bcbb06d903
Sebastian Ries <sr-github@kekze.de>
2018-01-26 10:29:31 +0100

    added relais configuration refactor variable names

--- schachtentwaesserung/schachtentwaesserung.ino
+++ schachtentwaesserung/schachtentwaesserung.ino
@@ -1,7 +1,8 @@
 
 // Configure Pins
-int trigger=7; //Trigger-Pin des Ultraschallsensors an Pin7 des Arduino-Boards 
-int echo=6; // Echo-Pim des Ultraschallsensors an Pin6 des Arduino-Boards 
+int pin_distance_trigger=7; //pin_distance_trigger-Pin des Ultraschallsensors an Pin7 des Arduino-Boards 
+int pin_distance_echo=6; // pin_distance_echo-Pim des Ultraschallsensors an Pin6 des Arduino-Boards
+int pin_relais=8; //Relais-Trigger on Pin8 des Arduino-Boards
 
 // define constants
 int messintervall=10000; //Messintervall in Millisekunden
@@ -15,19 +16,21 @@ long entfernung=0; // Das Wort „entfernung“ ist jetzt die variable, unter de
 void setup()
 {
   Serial.begin (9600); //Serielle kommunikation starten, damit man sich später die Werte am serial monitor ansehen kann.
-  pinMode(trigger, OUTPUT); // Trigger-Pin ist ein Ausgang
-  pinMode(echo, INPUT); // Echo-Pin ist ein Eingang
+  pinMode(pin_distance_trigger, OUTPUT); // pin_distance_trigger-Pin ist ein Ausgang
+  pinMode(pin_distance_echo, INPUT); // pin_distance_echo-Pin ist ein Eingang
+  pinMode(pin_relais, OUTPUT);
+  digitalWrite(pin_relais, LOW); //disable pump on startup
 }
 
 
 void loop()
 {
-  digitalWrite(trigger, LOW); //Hier nimmt man die Spannung für kurze Zeit vom Trigger-Pin, damit man später beim senden des Trigger-Signals ein rauschfreies Signal hat.
+  digitalWrite(pin_distance_trigger, LOW); //Hier nimmt man die Spannung für kurze Zeit vom pin_distance_trigger-Pin, damit man später beim senden des pin_distance_trigger-Signals ein rauschfreies Signal hat.
   delay(5); //Dauer: 5 Millisekunden
-  digitalWrite(trigger, HIGH); //Jetzt sendet man eine Ultraschallwelle los.
+  digitalWrite(pin_distance_trigger, HIGH); //Jetzt sendet man eine Ultraschallwelle los.
   delay(10); //Dieser „Ton“ erklingt für 10 Millisekunden.
-  digitalWrite(trigger, LOW);//Dann wird der „Ton“ abgeschaltet.
-  dauer = pulseIn(echo, HIGH); //Mit dem Befehl „pulseIn“ zählt der Mikrokontroller die Zeit in Mikrosekunden, bis der Schall zum Ultraschallsensor zurückkehrt.
+  digitalWrite(pin_distance_trigger, LOW);//Dann wird der „Ton“ abgeschaltet.
+  dauer = pulseIn(pin_distance_echo, HIGH); //Mit dem Befehl „pulseIn“ zählt der Mikrokontroller die Zeit in Mikrosekunden, bis der Schall zum Ultraschallsensor zurückkehrt.
   entfernung = (dauer/2) * 0.03432; //Nun berechnet man die Entfernung in Zentimetern. Man teilt zunächst die Zeit durch zwei (Weil man ja nur eine Strecke berechnen möchte und nicht die Strecke hin- und zurück). Den Wert multipliziert man mit der Schallgeschwindigkeit in der Einheit Zentimeter/Mikrosekunde und erhält dann den Wert in Zentimetern.
   if (entfernung >= 500 || entfernung <= 0) //Wenn die gemessene Entfernung über 500cm oder unter 0cm liegt,…
   {
@@ -42,11 +45,13 @@ void loop()
     {
       Serial.println("AN");
       // starte Pumpe
+      digitalWrite(pin_relais, HIGH);
     }
     else
     {
       Serial.println("AUS");
-      // stope Pumpe
+      // stoppe Pumpe
+      digitalWrite(pin_relais, LOW);
     }
   }
   delay(messintervall); //Das delay von einer Sekunde sorgt in ca. jeder neuen Sekunde für einen neuen Messwert.
